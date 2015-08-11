c59d372cd9812ec2e4c52185be3124493bf6ca4a
Eternity921020 <1019459067@qq.com>
2015-08-11 17:03:36 +0800

    拍照
    利用AVFoundation 进行两种方式的拍照

---
+++ TakePhoto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TakePhoto
+//
+//  Created by Eternity° on 15/8/11.
+//  Copyright (c) 2015年 Eternity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
