abeafdb6f5647ade05752711e3914a59bc5923ca
Eternity921020 <1019459067@qq.com>
2015-08-11 21:35:21 +0800

    能进行拍照，但是不能处理“BSXPCMessage received error for message: Connection interrupted”警告

--- TakePhoto/ViewCotroller/CaptureVideoPreviewLayer.m
+++ TakePhoto/ViewCotroller/CaptureVideoPreviewLayer.m
@@ -9,7 +9,7 @@
 #import "CaptureVideoPreviewLayer.h"
 #import <AVFoundation/AVFoundation.h>
 
-@interface CaptureVideoPreviewLayer ()
+@interface CaptureVideoPreviewLayer ()<AVCaptureVideoDataOutputSampleBufferDelegate>
 @property (weak, nonatomic) IBOutlet UIView *viewContrainer;
 
 @property (nonatomic, strong)AVCaptureDevice *captureDevice;
@@ -37,8 +37,6 @@
 - (AVCaptureVideoPreviewLayer *)captureVideoPreviewLayer {
     if (!_captureVideoPreviewLayer) {
         _captureVideoPreviewLayer = [[AVCaptureVideoPreviewLayer alloc]initWithSession:self.captureSession];
-//        _captureVideoPreviewLayer.frame = self.viewContrainer.frame;
-//        _captureVideoPreviewLayer.position = self.view.center;
         [_captureVideoPreviewLayer setVideoGravity:AVLayerVideoGravityResizeAspectFill];
     }
     return _captureVideoPreviewLayer;
@@ -59,18 +57,66 @@
 }
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [self.captureSession beginConfiguration];
     //4、输出设备
     NSError *error = nil;
     AVCaptureDeviceInput *captureDeviceInput = [[AVCaptureDeviceInput alloc]initWithDevice:self.captureDevice error:&error];
     if (error) {
         NSLog(@"暂不支持照相功能");
     }
-    
     if ([self.captureSession canAddInput:captureDeviceInput]) {
         [self.captureSession addInput:captureDeviceInput];
+    }
+
+    //5、视频输出
+    AVCaptureVideoDataOutput *captureVideoDataOutput = [[AVCaptureVideoDataOutput alloc]init];
+    [captureVideoDataOutput setAlwaysDiscardsLateVideoFrames:YES];//丢弃延迟的帧
+    [captureVideoDataOutput setVideoSettings:@{(id)kCVPixelBufferPixelFormatTypeKey:@(kCVPixelFormatType_420YpCbCr8BiPlanarFullRange)}];
+    [captureVideoDataOutput setSampleBufferDelegate:self queue:dispatch_get_main_queue()];
+    if ([self.captureSession canAddOutput:captureVideoDataOutput]) {
+        [self.captureSession addOutput:captureVideoDataOutput];
     }
     
     [self.captureSession commitConfiguration];
     [self.captureSession startRunning];
+}
+- (IBAction)onBtnTakePhoto:(UIButton *)sender {
+    [self.captureSession stopRunning];
+}
+- (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection {
+    CVPixelBufferRef pixelBuffer = (CVPixelBufferRef)CMSampleBufferGetImageBuffer(sampleBuffer);
+    CVPixelBufferLockBaseAddress(pixelBuffer, 0);
+    
+    [self imageFromSampleBuffer:pixelBuffer];
+    
+    CVPixelBufferUnlockBaseAddress(pixelBuffer, 0);
+
+}
+// pixelBuffer transform image
+- (UIImage *)imageFromSampleBuffer:(CVPixelBufferRef)pixelBuffer {
+    
+    CIImage *image = [CIImage imageWithCVPixelBuffer:pixelBuffer];
+//    CIContext *temporaryContext = [CIContext contextWithOptions:@{kCIContextUseSoftwareRenderer: @(YES)}];
+    CIContext *temporaryContext = [CIContext contextWithOptions:nil];
+    CGImageRef videoImage = [temporaryContext createCGImage:image fromRect:CGRectMake(0, 0,CVPixelBufferGetWidth(pixelBuffer),CVPixelBufferGetHeight(pixelBuffer) )];
+    UIImage *imageGet = [UIImage imageWithCGImage:videoImage scale:1.0 orientation:UIImageOrientationRight];
+    CGImageRelease(videoImage);
+    
+    UIImage *imagePortrait = [self cropImage:imageGet atRect:CGRectMake(0, 0, imageGet.size.width, imageGet.size.height)];
+    return imagePortrait;
+}
+//change image's orientation
+- (UIImage *)cropImage:(UIImage *)image atRect:(CGRect) rect
+{
+    UIImage *croppedImage = nil;
+    UIGraphicsBeginImageContext(rect.size);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGRect drawRect = CGRectMake(-rect.origin.x, -rect.origin.y, image.size.width, image.size.height);
+    CGContextClipToRect(context, CGRectMake(0, 0, rect.size.width, rect.size.height));
+    [image drawInRect:drawRect];
+    croppedImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return croppedImage;
 }
 @end
